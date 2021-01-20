d1ae2982a90b74a4f775240cd66b0529034b4ad2
Juan Carlos Condori Machicado <jcondori@agetic.gob.bo>
2021-01-20 12:19:50 -0400

    Adición de logger pino y pino-pretty

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,8 +5,19 @@ import { UsuarioModule } from './modules/usuario/usuario.module';
 import { AutenticacionModule } from './modules/autenticacion/autenticacion.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from 'src/config/typeorm.config';
+import { LoggerModule } from "nestjs-pino";
+
 @Module({
   imports: [
+    LoggerModule.forRoot({
+     pinoHttp: process.env.NODE_ENV === 'production' ? {timestamp: () => `,"time":"${new Date(Date.now()).toISOString()}"`} : {
+       prettyPrint: {
+         colorize: true,
+         levelFirst: true,
+         translateTime: true
+       }
+     }
+    }),
     UsuarioModule, 
     AutenticacionModule, 
     TypeOrmModule.forRoot(typeOrmConfig)],
