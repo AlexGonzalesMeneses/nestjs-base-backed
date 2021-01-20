632f4ee370f323ad833e6883c0308991e3f903e7
Armando Alvarez <falvarez@agetic.gob.bo>
2021-01-20 15:12:16 -0400

    ajustes segipPersona.dto.ts

--- src/external/iop/segip/segipPersona.dto.ts
+++ src/external/iop/segip/segipPersona.dto.ts
@@ -1,22 +1,16 @@
 import { IsNotEmpty, Matches } from 'class-validator';
 
 export class SegipPersonaDTO {
-
-    Complemento: string;
-
-    @IsNotEmpty()
-    NumeroDocumento: string;
-
-    @IsNotEmpty()
-    Nombres: string;
-
-    @IsNotEmpty()
-    PrimerApellido: string;
-
-    @IsNotEmpty()
-    SegundoApellido: string;
-
-    @IsNotEmpty()
-    @Matches(/^(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])[- /.](19|20)\d\d$/)
-    FechaNacimiento: string;
+  Complemento: string;
+  @IsNotEmpty()
+  NumeroDocumento: string;
+  @IsNotEmpty()
+  Nombres: string;
+  @IsNotEmpty()
+  PrimerApellido: string;
+  @IsNotEmpty()
+  SegundoApellido: string;
+  @IsNotEmpty()
+  @Matches(/^(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])[- /.](19|20)\d\d$/)
+  FechaNacimiento: string;
 }
