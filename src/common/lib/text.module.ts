4d34e9e0b5817a620c3fd8eef0cc053b9f7d413b
alvaro <almamani@agetic.gob.bo>
2021-01-21 11:04:52 -0400

    arreglo warning eslint

--- src/common/lib/text.module.ts
+++ src/common/lib/text.module.ts
@@ -1,14 +1,14 @@
 import * as crypto from 'crypto';
 
 export const encrypt = (password: string): string =>
-    crypto.createHash('sha256')
-          .update(password)
-          .digest('hex');
+  crypto.createHash('sha256').update(password).digest('hex');
 
 export const nano = (template: string, data: string): string =>
-    template.replace(/\{([\w.]*)\}/g, function (str, key) {
-        let keys = key.split('.');
-        let v = data[keys.shift()];
-        for ( let i: number = 0, l = keys.length; i < l; i++ ) { v = v[keys[i]]; }
-        return (typeof v !== 'undefined' && v !== null) ? v : '';
-    });
+  template.replace(/\{([\w.]*)\}/g, function (str, key) {
+    const keys = key.split('.');
+    let v = data[keys.shift()];
+    for (let i = 0, l = keys.length; i < l; i++) {
+      v = v[keys[i]];
+    }
+    return typeof v !== 'undefined' && v !== null ? v : '';
+  });
