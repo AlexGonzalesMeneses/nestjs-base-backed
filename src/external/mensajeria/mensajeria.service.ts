57c2de70a04f47fce10abb67b7169898466e3d75
alvaro <almamani@agetic.gob.bo>
2021-01-21 09:14:46 -0400

    refactor + test mensajeria service

--- src/external/mensajeria/mensajeria.service.ts
+++ src/external/mensajeria/mensajeria.service.ts
@@ -1,6 +1,4 @@
 import { HttpException, HttpService, Injectable } from '@nestjs/common';
-import { AxiosResponse } from 'axios';
-import { Observable } from 'rxjs';
 import { catchError, map } from 'rxjs/operators';
 
 @Injectable()
@@ -12,21 +10,22 @@ export class MensajeriaService {
    * @param cellphone Numero de celular
    * @param content contenido
    */
-  async sendSms(
-    cellphone: string,
-    content: string,
-  ): Promise<Observable<AxiosResponse<any>>> {
+  async sendSms(cellphone: string, content: string) {
     const smsBody = {
       para: [cellphone],
       contenido: content,
     };
 
-    return this.httpService.post('/correo', smsBody).pipe(
-      map((response) => response.data),
-      catchError((error) => {
-        throw new HttpException(error.response.data, error.response.status);
-      }),
-    );
+    const response = await this.httpService
+      .post('/correo', smsBody)
+      .pipe(
+        map((response) => response.data),
+        catchError((error) => {
+          throw new HttpException(error.response.data, error.response.status);
+        }),
+      )
+      .toPromise();
+    return response;
   }
 
   /**
@@ -35,48 +34,56 @@ export class MensajeriaService {
    * @param subject asunto
    * @param content contenido
    */
-  async sendEmail(
-    email: string,
-    subject: string,
-    content: string,
-  ): Promise<Observable<AxiosResponse<any>>> {
+  async sendEmail(email: string, subject: string, content: string) {
     const emailBody = {
       para: [email],
       asunto: subject,
       contenido: content,
     };
 
-    return this.httpService.post('/correo', emailBody).pipe(
-      map((response) => response.data),
-      catchError((error) => {
-        throw new HttpException(error.response.data, error.response.status);
-      }),
-    );
+    const response = this.httpService
+      .post('/correo', emailBody)
+      .pipe(
+        map((response) => response.data),
+        catchError((error) => {
+          throw new HttpException(error.response.data, error.response.status);
+        }),
+      )
+      .toPromise();
+    return response;
   }
 
   /**
    * Metodo para obtener el estado de un sms enviado
    * @param id Identificador de solicitud sms
    */
-  async getReportSms(id: string): Promise<Observable<AxiosResponse<any>>> {
-    return this.httpService.get(`/sms/reporte/${id}`).pipe(
-      map((response) => response.data),
-      catchError((error) => {
-        throw new HttpException(error.response.data, error.response.status);
-      }),
-    );
+  async getReportSms(id: string) {
+    const response = this.httpService
+      .get(`/sms/reporte/${id}`)
+      .pipe(
+        map((response) => response.data),
+        catchError((error) => {
+          throw new HttpException(error.response.data, error.response.status);
+        }),
+      )
+      .toPromise();
+    return response;
   }
 
   /**
    * Metodo para obtener el estado de un correo enviado
    * @param id Identificador de solicitud correo
    */
-  async getReportEmail(id: string): Promise<Observable<AxiosResponse<any>>> {
-    return this.httpService.get(`/correo/reporte/${id}`).pipe(
-      map((response) => response.data),
-      catchError((error) => {
-        throw new HttpException(error.response.data, error.response.status);
-      }),
-    );
+  async getReportEmail(id: string) {
+    const response = this.httpService
+      .get(`/correo/reporte/${id}`)
+      .pipe(
+        map((response) => response.data),
+        catchError((error) => {
+          throw new HttpException(error.response.data, error.response.status);
+        }),
+      )
+      .toPromise();
+    return response;
   }
 }
