9be76231642bf443ad0dc3e0dc33eb10873eeac9
ecanqui <ecanqui@agetic.gob.bo>
2021-01-21 12:45:30 -0400

    ajuste controlador

--- src/modules/autenticacion/autenticacion.controller.ts
+++ src/modules/autenticacion/autenticacion.controller.ts
@@ -1,9 +1,6 @@
-import { Controller, Get, Post, Request, Res, UseGuards } from '@nestjs/common';
-import { Response } from 'express';
-import { JwtAuthGuard } from './guards/jwt-auth.guard';
+import { Controller, Post, Request, UseGuards } from '@nestjs/common';
 import { LocalAuthGuard } from './guards/local-auth.guard';
 import { AutenticacionService } from './autenticacion.service';
-import { OidcAuthGuard } from './guards/oidc-auth.guard';
 
 @Controller()
 export class AutenticacionController {
@@ -12,27 +9,6 @@ export class AutenticacionController {
   @UseGuards(LocalAuthGuard)
   @Post('auth')
   async login(@Request() req) {
-    console.log('authController------------------------------q');
     return this.autenticacionService.autenticar(req.user);
-  }
-
-  @UseGuards(OidcAuthGuard)
-  @Get('ciudadania-auth')
-  async loginCiudadania() {
-    //
-  }
-
-  @UseGuards(OidcAuthGuard)
-  @Get('ciudadania-callback')
-  async loginCiudadaniaCallback(@Request() req, @Res() res: Response) {
-    console.log('authController-------------------user', req.user);
-    res.json({ mensaje: 'ok' });
-  }
-
-  @UseGuards(JwtAuthGuard)
-  @Get('profile')
-  getProfile(@Request() req) {
-    console.log('authController------------------------------p', req.user);
-    return req.user;
   }
 }
