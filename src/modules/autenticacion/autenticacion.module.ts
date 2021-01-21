97bd476fa8ae635f93faa9b525e0788d7a71ede5
ecanqui <ecanqui@agetic.gob.bo>
2021-01-21 09:14:52 -0400

    strategy jwt

--- src/modules/autenticacion/autenticacion.module.ts
+++ src/modules/autenticacion/autenticacion.module.ts
@@ -1,21 +1,27 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { UsuarioModule } from '../usuario/usuario.module';
 import { AutenticacionController } from './autenticacion.controller';
 import { AutenticacionService } from './autenticacion.service';
+import { JwtStrategy } from './strategies/jwt.strategy';
 import { LocalStrategy } from './strategies/local.strategy';
-import { jwtConstants } from './constants';
 
 @Module({
   imports: [
-    JwtModule.register({
-      secret: jwtConstants.secret,
-      signOptions: { expiresIn: jwtConstants.expiresIn },
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get('JWT_SECRET'),
+        signOptions: { expiresIn: configService.get('JWT_EXPIRES_IN') },
+      }),
     }),
     UsuarioModule,
+    ConfigModule,
   ],
   controllers: [AutenticacionController],
-  providers: [AutenticacionService, LocalStrategy],
+  providers: [AutenticacionService, LocalStrategy, JwtStrategy],
   exports: [AutenticacionService],
 })
 export class AutenticacionModule {}
--- src/modules/autenticacion/constants.ts
+++
@@ -1,6 +0,0 @@
-import * as dotenv from 'dotenv';
-
-export const jwtConstants = {
-  secret: dotenv.config().parsed.JWT_SECRET,
-  expiresIn: dotenv.config().parsed.JWT_EXPIRES_IN,
-};
