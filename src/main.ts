9c1a6341d80c6c66b064b5b1003e2b48c6fc3a4e
alvaro <almamani@agetic.gob.bo>
2021-01-21 12:33:57 -0400

    adicion paquetes seguridad

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import * as helmet from 'helmet';
+import * as csurf from 'csurf';
+
 import {
   SWAGGER_API_DESCRIPTION,
   SWAGGER_API_NAME,
@@ -12,6 +15,7 @@ import {
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
+
   const config = new DocumentBuilder()
     .setTitle(SWAGGER_API_NAME)
     .setDescription(SWAGGER_API_DESCRIPTION)
@@ -19,8 +23,12 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup(SWAGGER_API_ROOT, app, document);
-
   app.setGlobalPrefix(configService.get('PATH_SUBDOMAIN'));
+
+  app.enableCors();
+  app.use(helmet());
+  app.use(csurf());
+
   const port = configService.get('PORT');
   await app.listen(port);
   console.log(`Aplicación iniciada en el puerto ${port}`);
