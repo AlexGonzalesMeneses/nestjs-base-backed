e3b5a20581ecf36e7f58f5c7f186141cf5dafeb1
alvaro <almamani@agetic.gob.bo>
2021-01-21 12:24:31 -0400

    adicion de constantes para swagger

--- src/common/constants/index.ts
+++ src/common/constants/index.ts
@@ -1,1 +1,8 @@
+// status
 export const STATUS_INACTIVE = 'INACTIVO';
+
+// swagger config
+export const SWAGGER_API_ROOT = 'api/docs';
+export const SWAGGER_API_NAME = 'Proyecto base';
+export const SWAGGER_API_DESCRIPTION = 'Documentación de proyecto base';
+export const SWAGGER_API_CURRENT_VERSION = '1.0';
--- src/main.ts
+++ src/main.ts
@@ -2,17 +2,23 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import {
+  SWAGGER_API_DESCRIPTION,
+  SWAGGER_API_NAME,
+  SWAGGER_API_CURRENT_VERSION,
+  SWAGGER_API_ROOT,
+} from './common/constants';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const config = new DocumentBuilder()
-    .setTitle('Base Backend')
-    .setDescription('Documentación base')
-    .setVersion('1.0')
+    .setTitle(SWAGGER_API_NAME)
+    .setDescription(SWAGGER_API_DESCRIPTION)
+    .setVersion(SWAGGER_API_CURRENT_VERSION)
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup(SWAGGER_API_ROOT, app, document);
 
   app.setGlobalPrefix(configService.get('PATH_SUBDOMAIN'));
   const port = configService.get('PORT');
