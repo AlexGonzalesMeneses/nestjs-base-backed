b554b45ced1291364a7fae355a8bf257f437dfa1
ramiro <rhuayller@agetic.gob.bo>
2021-01-27 11:39:30 -0400

    wrap de respuesta ok

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,7 @@ import {
   SWAGGER_API_CURRENT_VERSION,
   SWAGGER_API_ROOT,
 } from './common/constants';
+import { WrapResponseInterceptor } from './common/interceptors/wrap-response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -41,6 +42,7 @@ async function bootstrap() {
   app.use(helmet());
 
   app.setGlobalPrefix(configService.get('PATH_SUBDOMAIN'));
+  app.useGlobalInterceptors(new WrapResponseInterceptor());
   const port = configService.get('PORT');
   await app.listen(port);
   console.log(
