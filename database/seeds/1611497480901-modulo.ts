1084478c22295f12f2cfb654ff371903201a8dec
ramiro <ramihertz@gmail.com>
2021-01-26 22:19:10 -0400

    agregando parametros y entidades en modulos para menu de admin

--- database/seeds/1611497480901-modulo.ts
+++ database/seeds/1611497480901-modulo.ts
@@ -5,6 +5,10 @@ export class modulo1611497480901 implements MigrationInterface {
                               VALUES('b9d07e44-97a1-40bd-85be-99ed16b435c9', 'configuracion','/configuracion', 'configuracion', 'settings', 'ACTIVO')`);
     await queryRunner.query(`INSERT INTO modulo (id, nombre, url, label, icono, estado)
                             VALUES('d561bd91-c406-4dd4-9d3f-fd9c19f5aba2', 'usuarios','/usuarios', 'usuarios', 'person', 'ACTIVO')`);
+    await queryRunner.query(`INSERT INTO modulo (id, nombre, url, label, icono, estado)
+                            VALUES('d561bd91-c406-4dd4-9d3f-fd9c19f5aba3', 'entidades','/entidades', 'entidades', 'business', 'ACTIVO')`);
+    await queryRunner.query(`INSERT INTO modulo (id, nombre, url, label, icono, estado)
+                            VALUES('d561bd91-c406-4dd4-9d3f-fd9c19f5aba4', 'parametros','/parametros', 'parametros', 'setup', 'ACTIVO')`);                            
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
--- database/seeds/1611514541659-rol-modulo.ts
+++ database/seeds/1611514541659-rol-modulo.ts
@@ -5,8 +5,14 @@ export class rolModulo1611514541659 implements MigrationInterface {
     await queryRunner.query(`INSERT INTO rol_modulo (id, id_rol, id_modulo, estado)
                             VALUES('${uuid.v4()}', '526b78e9-2434-4d9d-b10e-c59a37fbde13', 'd561bd91-c406-4dd4-9d3f-fd9c19f5aba2','ACTIVO')`);
 
+    // await queryRunner.query(`INSERT INTO rol_modulo (id, id_rol, id_modulo, estado)
+    //                         VALUES('${uuid.v4()}', '526b78e9-2434-4d9d-b10e-c59a37fbde13', 'b9d07e44-97a1-40bd-85be-99ed16b435c9', 'ACTIVO')`);
+
     await queryRunner.query(`INSERT INTO rol_modulo (id, id_rol, id_modulo, estado)
-                            VALUES('${uuid.v4()}', '526b78e9-2434-4d9d-b10e-c59a37fbde13', 'b9d07e44-97a1-40bd-85be-99ed16b435c9', 'ACTIVO')`);
+                            VALUES('${uuid.v4()}', '526b78e9-2434-4d9d-b10e-c59a37fbde13', 'd561bd91-c406-4dd4-9d3f-fd9c19f5aba3', 'ACTIVO')`);
+
+    await queryRunner.query(`INSERT INTO rol_modulo (id, id_rol, id_modulo, estado)
+                            VALUES('${uuid.v4()}', '526b78e9-2434-4d9d-b10e-c59a37fbde13', 'd561bd91-c406-4dd4-9d3f-fd9c19f5aba4', 'ACTIVO')`);                            
   }
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`TRUNCATE TABLE rol_modulo;`);
