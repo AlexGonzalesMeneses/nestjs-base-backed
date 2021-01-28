4733539640524c2435c9ee728b114482dc3a9631
Armando Alvarez <falvarez@agetic.gob.bo>
2021-01-28 12:51:49 -0400

    reportes

--- src/modules/reporte/reporte.controller.ts
+++ src/modules/reporte/reporte.controller.ts
@@ -1,10 +1,10 @@
 import {
   Controller,
-  Header,
   HttpCode,
   HttpStatus,
   Get,
   Res,
+  Param,
 } from '@nestjs/common';
 import { Response } from 'express';
 import { ReporteService } from './reporte.service';
@@ -13,12 +13,10 @@ import { ReporteService } from './reporte.service';
 export class ReporteController {
   constructor(private reporteService: ReporteService) {}
 
-  @Get('generar/buffer')
+  @Get('generar/:tipo')
   @HttpCode(HttpStatus.OK)
-  @Header('Content-Type', 'application/pdf')
-  @Header('Content-Disposition', 'attachment; filename=test.pdf')
-  async generar(@Res() res: Response) {
-    const nombreArchivo = 'reporte.pdf';
+  async generar(@Res() res: Response, @Param('tipo') tipo: string) {
+    const nombreArchivo = `reporte${tipo}.pdf`;
     const plantillaHtml = 'src/templates/default.html';
     const rutaGuardadoPdf = `${process.env.PDF_PATH}${nombreArchivo}`;
     const configPagina = {
@@ -31,7 +29,7 @@ export class ReporteController {
       output: rutaGuardadoPdf,
     };
     const parametros = {
-      titulo: 'Pagina de prueba',
+      titulo: `Pagina de Prueba ${tipo}`,
     };
     await this.reporteService.generar(
       plantillaHtml,
@@ -39,36 +37,19 @@ export class ReporteController {
       rutaGuardadoPdf,
       configPagina,
     );
-    return res.download(rutaGuardadoPdf);
-  }
 
-  @Get('generar/base64')
-  @HttpCode(HttpStatus.OK)
-  async generarBase64(@Res() res: Response) {
-    const nombreArchivo = 'reportebase64.pdf';
-    const plantillaHtml = 'src/templates/default.html';
-    const rutaGuardadoPdf = `${process.env.PDF_PATH}${nombreArchivo}`;
-    const configPagina = {
-      pageSize: 'Letter',
-      orientation: 'portrait',
-      marginLeft: '0.5cm',
-      marginRight: '0.5cm',
-      marginTop: '0.5cm',
-      marginBottom: '0.5cm',
-      output: rutaGuardadoPdf,
-    };
-    const parametros = {
-      titulo: 'Pagina de prueba Base64',
-    };
-    await this.reporteService.generar(
-      plantillaHtml,
-      parametros,
-      rutaGuardadoPdf,
-      configPagina,
-    );
-    const resultado = await this.reporteService.descargarBase64(
-      rutaGuardadoPdf,
-    );
-    res.status(HttpStatus.OK).json({ data: resultado });
+    if (tipo === 'base64') {
+      const resultado = await this.reporteService.descargarBase64(
+        rutaGuardadoPdf,
+      );
+      res.status(HttpStatus.OK).json({ data: resultado });
+    } else {
+      res.setHeader('Content-Type', 'application/pdf');
+      res.setHeader(
+        'Content-Disposition',
+        `attachment; filename=${nombreArchivo}`,
+      );
+      res.download(rutaGuardadoPdf);
+    }
   }
 }
