b00604d8984a53b6b3e028acd050741db0244a03
alvaro <almamani@agetic.gob.bo>
2021-02-08 16:47:53 -0400

    refactor dto para paginacion

--- src/common/dto/paginacion-query.dto.ts
+++ src/common/dto/paginacion-query.dto.ts
@@ -1,11 +1,59 @@
-import { IsOptional, IsPositive } from 'class-validator';
+import { ApiPropertyOptional } from '@nestjs/swagger';
+import { Type } from 'class-transformer';
+import {
+  IsEnum,
+  IsInt,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  Max,
+  Min,
+} from 'class-validator';
+
+import { Orden } from '../constants/orden';
+
+const LIMITE_MIN = 10;
+const LIMITE_MAX = 50;
+const PAGINA_MIN = 1;
 
 export class PaginacionQueryDto {
+  @ApiPropertyOptional({
+    minimum: LIMITE_MIN,
+    maximum: LIMITE_MAX,
+    default: LIMITE_MIN,
+  })
+  @Type(() => Number)
+  @IsInt()
+  @Min(LIMITE_MIN)
+  @Max(LIMITE_MAX)
   @IsOptional()
-  @IsPositive()
-  limite: number;
+  readonly limite?: number = LIMITE_MIN;
 
+  @ApiPropertyOptional({
+    minimum: 1,
+    default: 1,
+  })
+  @Type(() => Number)
+  @IsInt()
+  @Min(PAGINA_MIN)
   @IsOptional()
-  @IsPositive()
-  pagina: number;
+  readonly pagina?: number = PAGINA_MIN;
+
+  @ApiPropertyOptional()
+  @IsNotEmpty()
+  @IsOptional()
+  @IsString()
+  readonly filtro?: string;
+
+  @ApiPropertyOptional({
+    enum: Orden,
+    default: Orden.ASC,
+  })
+  @IsEnum(Orden)
+  @IsOptional()
+  readonly orden?: Orden = Orden.ASC;
+
+  get omitir(): number {
+    return (this.pagina - 1) * this.limite;
+  }
 }
