687ea5bea352925e9ee9e76601a1c63645761132
Adrian <dbarra@agetic.gob.bo>
2021-02-09 09:03:34 -0400

    Modificando icons

--- database/seeds/1611497480901-modulo.ts
+++ database/seeds/1611497480901-modulo.ts
@@ -2,13 +2,13 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 export class modulo1611497480901 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`INSERT INTO modulo (id, nombre, url, label, icono, estado)
-                              VALUES('b9d07e44-97a1-40bd-85be-99ed16b435c9', 'configuracion','/configuracion', 'configuracion', 'settings', 'ACTIVO')`);
+                              VALUES('b9d07e44-97a1-40bd-85be-99ed16b435c9', 'configuracion','/configuracion', 'configuracion', 'mdiCogOutline', 'ACTIVO')`);
     await queryRunner.query(`INSERT INTO modulo (id, nombre, url, label, icono, estado)
-                            VALUES('d561bd91-c406-4dd4-9d3f-fd9c19f5aba2', 'usuarios','/usuarios', 'usuarios', 'person', 'ACTIVO')`);
+                            VALUES('d561bd91-c406-4dd4-9d3f-fd9c19f5aba2', 'usuarios','/usuarios', 'usuarios', 'mdiAccountCog', 'ACTIVO')`);
     await queryRunner.query(`INSERT INTO modulo (id, nombre, url, label, icono, estado)
-                            VALUES('d561bd91-c406-4dd4-9d3f-fd9c19f5aba3', 'entidades','/entidades', 'entidades', 'business', 'ACTIVO')`);
+                            VALUES('d561bd91-c406-4dd4-9d3f-fd9c19f5aba3', 'entidades','/entidades', 'entidades', 'mdiDomain', 'ACTIVO')`);
     await queryRunner.query(`INSERT INTO modulo (id, nombre, url, label, icono, estado)
-                            VALUES('d561bd91-c406-4dd4-9d3f-fd9c19f5aba4', 'parametros','/parametros', 'parametros', 'setup', 'ACTIVO')`);                            
+                            VALUES('d561bd91-c406-4dd4-9d3f-fd9c19f5aba4', 'parametros','/parametros', 'parametros', 'mdiFileTree', 'ACTIVO')`);                            
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
