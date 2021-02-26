de0be2f7d75b9d19d4172a0beb6800266fadd86f
Ever Argollo <everfavioat@gmail.com>
2021-02-26 14:47:47 -0400

    agregados tags de id commit, rama y comentario, visibles en la ruta rest de estado

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,12 @@ RUN mkdir -p /home/node/app/node_modules && chown -R node:node /home/node/app
 WORKDIR /home/node/app
 COPY --chown=node:node . .
 USER node
+ARG CI_COMMIT_SHORT_SHA
+ARG CI_COMMIT_MESSAGE
+ARG CI_COMMIT_REF_NAME
+ENV CI_COMMIT_SHORT_SHA=${CI_COMMIT_SHORT_SHA} \
+    CI_COMMIT_MESSAGE=${CI_COMMIT_MESSAGE} \
+    CI_COMMIT_REF_NAME=${CI_COMMIT_REF_NAME}
 RUN npm set registry http://repositorio.agetic.gob.bo/nexus/repository/npmjs
 RUN npm set strict-ssl false
 RUN npm run build
@@ -12,16 +18,21 @@ FROM build AS production
 WORKDIR /home/node/app
 COPY --from=build --chown=node:node /home/node/app/dist .
 USER node
+RUN env
+CMD ["node", "/home/node/app/dist/src/main.js"]
 EXPOSE 3000
 
 FROM build AS testing
 WORKDIR /home/node/app
 USER node
+RUN env
 CMD ["npm", "run", "start:dev"]
 EXPOSE 3000
 
 FROM build AS development
 WORKDIR /home/node/app
 USER node
+RUN env
+RUN echo "variables dev"
 CMD ["npm", "run", "start:dev"]
 EXPOSE 3000
--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,19 +2,23 @@ import { Controller, Get, HttpStatus, Inject, Res } from '@nestjs/common';
 import { Response } from 'express';
 import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
 import { Logger } from 'winston';
+import { ConfigService } from '@nestjs/config';
 
 @Controller()
 export class AppController {
   constructor(
     @Inject(WINSTON_MODULE_PROVIDER) private readonly logger: Logger,
+    @Inject(ConfigService) private readonly configService: ConfigService,
   ) {}
 
   @Get('/estado')
   async verificarEstado(@Res() res: Response) {
     this.logger.log('estado()', AppController.name);
-
     return res.status(HttpStatus.OK).json({
-      mensaje: 'Servicio funcionando correctamente',
+      estado: 'Servicio funcionando correctamente',
+      commit_sha: this.configService.get('CI_COMMIT_SHORT_SHA'),
+      mensaje: this.configService.get('CI_COMMIT_MESSAGE'),
+      branch: this.configService.get('CI_COMMIT_REF_NAME'),
       hora: Math.floor(Date.now() / 1000),
     });
   }
