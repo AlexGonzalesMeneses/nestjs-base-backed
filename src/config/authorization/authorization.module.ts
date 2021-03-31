e29018b81dadb316b9d960e035aa4a4e06a649bd
alvaro <almamani@agetic.gob.bo>
2021-03-31 15:42:43 -0400

    adicion de politicas por defecto

--- src/config/authorization/authorization.module.ts
+++ src/config/authorization/authorization.module.ts
@@ -19,11 +19,40 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
             username: configService.get('DB_USERNAME'),
             password: configService.get('DB_PASSWORD'),
             database: configService.get('DB_DATABASE'),
+            logging: configService.get('NODE_ENV') === 'development',
           });
           const enforcer = await newEnforcer(
             join(__dirname, 'model.conf'),
             adapter,
           );
+          await enforcer.enableLog(true);
+          // TODO: politicas definir de donde se obtendra
+          const politicas = [
+            {
+              sujeto: 'admin',
+              objeto: '/status',
+              accion: 'GET',
+            },
+            {
+              sujeto: 'admin',
+              objeto: '/permisos',
+              accion: 'POST',
+            },
+            {
+              sujeto: 'admin',
+              objeto: '/permisos',
+              accion: 'GET',
+            },
+            {
+              sujeto: 'superadmin',
+              objeto: '/permisos',
+              accion: 'GET',
+            },
+          ];
+          for (let politica of politicas) {
+            const { sujeto, objeto, accion } = politica;
+            await enforcer.addPolicy(sujeto, objeto, accion);
+          }
           return enforcer;
         },
         inject: [ConfigService],
