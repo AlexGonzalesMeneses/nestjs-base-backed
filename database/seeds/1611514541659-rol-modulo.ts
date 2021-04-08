0fcb89b1864d8883935ec5c678f8d036235c7ea4
alvaro <almamani@agetic.gob.bo>
2021-04-08 15:40:56 -0400

    refactor seeds

--- database/seeds/1611514541659-rol-modulo.ts
+++ database/seeds/1611514541659-rol-modulo.ts
@@ -1,34 +1,65 @@
+import { TextService } from 'src/common/lib/text.service';
+import { Modulo } from 'src/core/authorization/entity/modulo.entity';
+import { RolModulo } from 'src/core/authorization/entity/rol-modulo.entity';
+import { Rol } from 'src/core/authorization/entity/rol.entity';
+import { RolEnum } from 'src/core/authorization/rol.enum';
 import { MigrationInterface, QueryRunner } from 'typeorm';
-import * as uuid from 'uuid';
 export class rolModulo1611514541659 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`INSERT INTO rol_modulo (id, id_rol, id_modulo, estado)
-                            VALUES('${uuid.v4()}', (SELECT id FROM rol WHERE rol = 'ADMINISTRADOR'), (SELECT id FROM modulo WHERE url='/usuarios'), 'ACTIVO')`);
+    const items = [
+      {
+        rol: TextService.textToUuid(RolEnum.ADMINISTRADOR),
+        modulo: TextService.textToUuid('usuarios'),
+      },
+      {
+        rol: TextService.textToUuid(RolEnum.ADMINISTRADOR),
+        modulo: TextService.textToUuid('entidades'),
+      },
+      {
+        rol: TextService.textToUuid(RolEnum.ADMINISTRADOR),
+        modulo: TextService.textToUuid('parametros'),
+      },
+      {
+        rol: TextService.textToUuid(RolEnum.TECNICO),
+        modulo: TextService.textToUuid('usuarios'),
+      },
+      {
+        rol: TextService.textToUuid(RolEnum.TECNICO),
+        modulo: TextService.textToUuid('entidades'),
+      },
+      {
+        rol: TextService.textToUuid(RolEnum.TECNICO),
+        modulo: TextService.textToUuid('parametros'),
+      },
+      {
+        rol: TextService.textToUuid(RolEnum.USUARIO),
+        modulo: TextService.textToUuid('usuarios'),
+      },
+      {
+        rol: TextService.textToUuid(RolEnum.USUARIO),
+        modulo: TextService.textToUuid('entidades'),
+      },
+      {
+        rol: TextService.textToUuid(RolEnum.ADMINISTRADOR),
+        modulo: TextService.textToUuid('politicas'),
+      },
+    ];
+    const rolesModulos = items.map((item) => {
+      const m = new Modulo();
+      m.id = item.modulo;
 
-    await queryRunner.query(`INSERT INTO rol_modulo (id, id_rol, id_modulo, estado)
-                            VALUES('${uuid.v4()}', (SELECT id FROM rol WHERE rol = 'ADMINISTRADOR'), (SELECT id FROM modulo WHERE url='/entidades'), 'ACTIVO')`);
+      const r = new Rol();
+      r.id = item.rol;
 
-    await queryRunner.query(`INSERT INTO rol_modulo (id, id_rol, id_modulo, estado)
-                            VALUES('${uuid.v4()}', (SELECT id FROM rol WHERE rol = 'ADMINISTRADOR'), (SELECT id FROM modulo WHERE url='/parametros'), 'ACTIVO')`);
-
-
-    await queryRunner.query(`INSERT INTO rol_modulo (id, id_rol, id_modulo, estado)
-                            VALUES('${uuid.v4()}', (SELECT id FROM rol WHERE rol = 'ENTIDAD'), (SELECT id FROM modulo WHERE url='/usuarios'), 'ACTIVO')`);
-
-    await queryRunner.query(`INSERT INTO rol_modulo (id, id_rol, id_modulo, estado)
-                            VALUES('${uuid.v4()}', (SELECT id FROM rol WHERE rol = 'ENTIDAD'), (SELECT id FROM modulo WHERE url='/entidades'), 'ACTIVO')`);
-
-    await queryRunner.query(`INSERT INTO rol_modulo (id, id_rol, id_modulo, estado)
-                            VALUES('${uuid.v4()}', (SELECT id FROM rol WHERE rol = 'ENTIDAD'), (SELECT id FROM modulo WHERE url='/parametros'), 'ACTIVO')`);
-
-
-    await queryRunner.query(`INSERT INTO rol_modulo (id, id_rol, id_modulo, estado)
-                            VALUES('${uuid.v4()}', (SELECT id FROM rol WHERE rol = 'USUARIO'), (SELECT id FROM modulo WHERE url='/usuarios'), 'ACTIVO')`);
-
-    await queryRunner.query(`INSERT INTO rol_modulo (id, id_rol, id_modulo, estado)
-                            VALUES('${uuid.v4()}', (SELECT id FROM rol WHERE rol = 'USUARIO'), (SELECT id FROM modulo WHERE url='/entidades'), 'ACTIVO')`);
+      const rm = new RolModulo();
+      rm.id = TextService.generateUuid();
+      rm.rol = r;
+      rm.modulo = m;
+      return rm;
+    });
+    await queryRunner.manager.save(rolesModulos);
   }
-  public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`TRUNCATE TABLE rol_modulo;`);
-  }
+
+  /* eslint-disable */
+  public async down(queryRunner: QueryRunner): Promise<void> {}
 }
--- docs/permisos.md
+++ docs/permisos.md
@@ -6,6 +6,7 @@
 |-|-|
 |GET|/estado|
 |POST|/auth|
+|GET|/ciudadania-auth|
 
 #### Rutas - Usuario loggeado
 |verbo|ruta|
@@ -13,27 +14,26 @@
 |GET|/usuarios/profile|
 |GET|/parametros/grupo/:grupo|
 |GET|/autorizacion/politicas/roles|
+|POST|/token|
+|GET|/logout|
 
 #### Rutas - Roles
 |ruta/roles|ADMINISTRADOR|TECNICO|
 |-|-|-|
-|GET /autorizacion/politicas|||
-|POST /autorizacion/politicas|||
-|POST /autorizacion/politicas/eliminar|||
-|GET /roles|x|||
-|GET /modulos|||
-|POST /token|||
-|GET /ciudadania-auth|||
+|GET /autorizacion/politicas|x||
+|POST /autorizacion/politicas|x||
+|POST /autorizacion/politicas/eliminar|x||
+|GET /roles|x||
+|GET /modulos|x||
 |GET /ciudadania-callback|||
-|GET /logout|||
 |GET /usuarios|x||
 |GET /usuarios/profile|x||
-|POST /usuarios|||
-|PATCH /usuarios/:id|||
-|DELETE /usuarios/:id|||
+|POST /usuarios|x||
+|PATCH /usuarios/:id|x||
+|DELETE /usuarios/:id|x||
 |GET /entidades|x|x|
-|POST /entidades|x||
-|PATCH /entidades/:id|x||
-|DELETE /entidades/:id|x||
-|GET /parametricas|x||
-|POST /parametricas|x||
+|POST /entidades|x|x|
+|PATCH /entidades/:id|x|x|
+|DELETE /entidades/:id|x|x|
+|GET /parametricas|x|x|
+|POST /parametricas|x|x|
