c218860a4e7afd2124ad7a74aca9b9c0df95c924
alvaro <almamani@agetic.gob.bo>
2021-04-09 12:46:41 -0400

    test para parametros

--- src/application/parametro/parametro.service.ts
+++ src/application/parametro/parametro.service.ts
@@ -3,8 +3,8 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { ParametroRepository } from './parametro.repository';
 import { Parametro } from './parametro.entity';
 import { ParametroDto } from './dto/parametro.dto';
-import { PaginacionQueryDto } from 'src/common/dto/paginacion-query.dto';
-import { totalRowsResponse } from 'src/common/lib/http.module';
+import { PaginacionQueryDto } from '../../common/dto/paginacion-query.dto';
+import { totalRowsResponse } from '../../common/lib/http.module';
 
 @Injectable()
 export class ParametroService {
