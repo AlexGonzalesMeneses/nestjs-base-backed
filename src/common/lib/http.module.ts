f7532309fc3b1dfb78db52fbef8af6627d781ef2
alvaro <almamani@agetic.gob.bo>
2021-04-09 10:14:41 -0400

    cambio en filtros de respuesta total y filas

--- src/common/lib/http.module.ts
+++ src/common/lib/http.module.ts
@@ -3,7 +3,8 @@ import { SuccessResponseDto } from '../dto/success-response.dto';
 import { TotalRowsResponseDto } from '../dto/total-rows-response.dto';
 
 export const totalRowsResponse = function (data): TotalRowsResponseDto {
-  return { total: data[1], rows: data[0] };
+  const [filas, total] = data;
+  return { total, filas };
 };
 
 export const successResponse = function (
