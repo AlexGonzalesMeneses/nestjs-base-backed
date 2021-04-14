2255d9bdfc2363967391b781fd7cf842bdaf7f63
Jpoma <jpoma@agetic.gob.bo>
2021-04-14 02:36:05 -0400

    incluyendo el stream para pino

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,7 +7,14 @@ import { ScheduleModule } from '@nestjs/schedule';
 import { CoreModule } from './core/core.module';
 import { ApplicationModule } from './application/application.module';
 import { LoggerModule } from 'nestjs-pino';
-import pino = require('pino');
+import pino from 'pino';
+import { createWriteStream } from 'pino-http-send';
+
+const stream = createWriteStream({
+  url: 'http://localhost:3005',
+  batchSize: 2,
+  log: true,
+});
 
 @Module({
   imports: [
@@ -18,10 +25,13 @@ import pino = require('pino');
     // LoggerModule.forRoot(), // This works! :D
     LoggerModule.forRoot({
       pinoHttp: {
-        logger: pino({
-          level: process.env.NODE_ENV !== 'production' ? 'debug' : 'info',
-          prettyPrint: process.env.NODE_ENV !== 'production',
-        }),
+        logger: pino(
+          {
+            level: process.env.NODE_ENV !== 'production' ? 'debug' : 'info',
+            prettyPrint: process.env.NODE_ENV !== 'production',
+          },
+          stream,
+        ),
       },
     }),
   ],
