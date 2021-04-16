c4616798b3672be9f6b47bba06b44fa990e70df1
alvaro <almamani@agetic.gob.bo>
2021-04-16 08:42:24 -0400

    filter para manejo de excepciones

--- src/common/filters/http-exception.filter.ts
+++ src/common/filters/http-exception.filter.ts
@@ -13,20 +13,42 @@ export class HttpExceptionFilter implements ExceptionFilter {
   catch(exception: HttpException, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
-    const status = exception.getStatus()
+    let status = exception.getStatus()
       ? exception.getStatus()
       : HttpStatus.INTERNAL_SERVER_ERROR;
-
+    const r = <any>exception.getResponse();
+    let errores = {};
+    Logger.error('[error] %j', JSON.stringify(r));
+    if (Array.isArray(r.message)) {
+      status = HttpStatus.UNPROCESSABLE_ENTITY;
+      const validationErrors = r.message;
+      errores = validationErrors;
+    }
     const errorResponse = {
       codigo: status,
       timestamp: new Date().toISOString(),
-      mensaje:
-        status !== HttpStatus.INTERNAL_SERVER_ERROR
-          ? exception.message || 'Ocurrio un error...'
-          : 'Error Interno',
+      mensaje: this.filterMessage(status),
+      datos: {
+        errores,
+      },
     };
-    // logger
-    Logger.error('[error] %j', JSON.stringify(errorResponse));
     response.status(status).json(errorResponse);
+  }
+  filterMessage(statusCode) {
+    let message;
+    switch (statusCode) {
+      case 404:
+        message = 'Error de validacion';
+        break;
+      case 401:
+        message = 'Usuario no autorizado';
+        break;
+      case 404:
+        message = 'Recurso no encontrado';
+        break;
+      default:
+        message = 'Ocurrio un error desconocido';
+    }
+    return message;
   }
 }
