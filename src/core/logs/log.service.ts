ca32a88e5460ef9d26b8cd6fc69c548f501fa580
Jpoma <jpoma@agetic.gob.bo>
2021-04-19 21:10:18 -0400

    lista de datos a ocultar de los logs

---
+++ src/core/logs/log.service.ts
@@ -0,0 +1,60 @@
+import { Injectable } from '@nestjs/common';
+import { id } from 'cls-rtracer';
+
+@Injectable()
+export class LogService {
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  // constructor() { }
+
+  static getLoggerConfig() {
+    return {
+      pinoHttp: this.getPinoHttpConfig(),
+    };
+  }
+
+  static getPinoHttpConfig() {
+    return {
+      name: process.env.npm_package_name || 'APP',
+      genReqId: (req) => {
+        return req.id || id();
+      },
+      serializers: {
+        req(req) {
+          req.body = req.raw.body;
+          return req;
+        },
+      },
+      redact: {
+        paths: (process.env.LOG_HIDE || '').split(' '),
+        censor: '*****',
+      },
+      level: 'info',
+      customLogLevel: (res, err) => {
+        if (err) {
+          if (res.statusCode >= 500) {
+            return 'error';
+          } else if (res.statusCode > 304 && res.statusCode < 500) {
+            return 'warn';
+          } else {
+            return 'error';
+          }
+        }
+        return 'info';
+      },
+      customSuccessMessage: (res) => {
+        if (res.statusCode <= 304) {
+          return `Request completed - ${res.statusMessage}`;
+        } else if (res.err) {
+          return `Request errored with - ${res.err.name} : reason:${res.err.message}`;
+        }
+      },
+      customAttributeKeys: {
+        req: `request`,
+        res: `response`,
+        err: `error`,
+        responseTime: `timeTakenForCompletion:ms`,
+      },
+      prettyPrint: process.env.NODE_ENV !== 'production',
+    };
+  }
+}
