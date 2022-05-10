05f6976169cce5e49a43ac017703b2ac10a772a4
Ion Villarreal <ivillarreal@agetic.gob.bo>
2022-05-10 18:47:28 -0400

    fix: corregida dependencia circular LogService en core.module que ya estaba importada en app.module

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -3,14 +3,13 @@ import { AuthenticationModule } from './authentication/authentication.module';
 import { AuthorizationModule } from './authorization/authorization.module';
 import { ConfigCoreModule } from './config/config.module';
 import { ExternalServicesModule } from './external-services/external.module';
-import { LogService } from './logs/log.service';
+
 @Module({
   imports: [
     ConfigCoreModule,
     ExternalServicesModule,
     AuthorizationModule,
     AuthenticationModule,
-    LogService,
   ],
   exports: [ExternalServicesModule],
 })
