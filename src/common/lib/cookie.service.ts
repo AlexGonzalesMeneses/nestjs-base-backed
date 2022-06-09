5cbf815ba80bbeb248b525075c9680059ca3fdfe
Ion Villarreal <ivillarreal@agetic.gob.bo>
2022-06-09 13:10:20 -0400

    fix: corregida definición de cookie segura ocasionado por la asignación de una cadena como booleano

--- src/common/lib/cookie.service.ts
+++ src/common/lib/cookie.service.ts
@@ -1,21 +1,13 @@
+import { CookieOptions } from 'express-serve-static-core';
+import { ConfigService } from '@nestjs/config';
+
 export class CookieService {
-  static makeConfig(configService) {
+  static makeConfig(configService: ConfigService): CookieOptions {
     const expiresIn = configService.get('REFRESH_TOKEN_EXPIRES_IN');
     const ttl = parseInt(expiresIn, 10);
     return {
       httpOnly: true,
-      // secure: configService.get('REFRESH_TOKEN_SECURE'),
-      expires: new Date(Date.now() + ttl),
-      path: configService.get('REFRESH_TOKEN_PATH'),
-    };
-  }
-
-  static makeConfigHttpOnlyFalse(configService) {
-    const expiresIn = configService.get('REFRESH_TOKEN_EXPIRES_IN');
-    const ttl = parseInt(expiresIn, 10);
-    return {
-      httpOnly: false,
-      // secure: configService.get('REFRESH_TOKEN_SECURE'),
+      secure: configService.get('REFRESH_TOKEN_SECURE') == 'true',
       expires: new Date(Date.now() + ttl),
       path: configService.get('REFRESH_TOKEN_PATH'),
     };
