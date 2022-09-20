38e221ee138a8390084c8e6183fa7ba9db3d086b
Alex Quispe <wilmer.quispe@agetic.gob.bo>
2022-09-20 14:01:06 -0400

    feat: servicio estado actualizado con más información

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,6 +3,8 @@ import { Response } from 'express'
 import { ConfigService } from '@nestjs/config'
 import { LoggerService } from './core/logger/logger.service'
 import { BaseController } from './common/base/base-controller'
+import packageJson from '../package.json'
+import dayjs from 'dayjs'
 
 @Controller()
 export class AppController extends BaseController {
@@ -16,10 +18,14 @@ export class AppController extends BaseController {
   @Get('/estado')
   async verificarEstado(@Res() res: Response) {
     return res.status(HttpStatus.OK).json({
+      servicio: packageJson.name,
+      version: packageJson.version,
+      entorno: process.env.NODE_ENV,
       estado: 'Servicio funcionando correctamente',
       commit_sha: this.configService.get('CI_COMMIT_SHORT_SHA'),
       mensaje: this.configService.get('CI_COMMIT_MESSAGE'),
       branch: this.configService.get('CI_COMMIT_REF_NAME'),
+      fecha: dayjs().format('DD/MM/YYYY HH:mm:ss'),
       hora: Math.floor(Date.now() / 1000),
     })
   }
