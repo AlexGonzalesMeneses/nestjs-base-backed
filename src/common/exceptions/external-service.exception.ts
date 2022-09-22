d041020fe8b85b864cf58a27ec44efc3cd6d5201
Alex Quispe <wilmer.quispe@agetic.gob.bo>
2022-09-22 12:17:12 -0400

    fix: cambios en ExternalServiceException para el registro de logs

--- src/common/exceptions/external-service.exception.ts
+++ src/common/exceptions/external-service.exception.ts
@@ -1,8 +1,14 @@
 import { HttpException, HttpStatus } from '@nestjs/common'
 
 export class ExternalServiceException extends HttpException {
-  constructor(service: string, errorMessage: string) {
-    const errMsg = `Error con el Servicio Web ${service}: ${errorMessage.toString()}`
-    super(errMsg, HttpStatus.PRECONDITION_FAILED)
+  constructor(service: string, err: unknown) {
+    super(
+      {
+        message: `Error con el Servicio Web ${service}`,
+        error: err,
+        errorStack: err instanceof Error ? err.stack : undefined,
+      },
+      HttpStatus.PRECONDITION_FAILED
+    )
   }
 }
