902f7f535f36933c91389d48d12240fbc8367e7c
Alvaro Martinez <alvaro.martinez@agetic.gob.bo>
2022-11-08 12:31:04 -0400

    feat: crud de roles

--- src/core/authorization/controller/rol.controller.ts
+++ src/core/authorization/controller/rol.controller.ts
@@ -1,8 +1,21 @@
-import { Controller, Get, UseGuards } from '@nestjs/common'
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+  Req,
+  UseGuards,
+} from '@nestjs/common'
 import { RolService } from '../service/rol.service'
 import { BaseController } from '../../../common/base/base-controller'
 import { CasbinGuard } from '../guards/casbin.guard'
 import { JwtAuthGuard } from '../../authentication/guards/jwt-auth.guard'
+import { CrearRolDto } from '../dto/crear-rol.dto'
+import { ParamIdDto } from 'src/common/dto/params-id.dto'
+import { PaginacionQueryDto } from 'src/common/dto/paginacion-query.dto'
 
 @UseGuards(JwtAuthGuard, CasbinGuard)
 @Controller('autorizacion/roles')
@@ -15,5 +28,50 @@ export class RolController extends BaseController {
   async listar() {
     const result = await this.rolService.listar()
     return this.successList(result)
+  }
+
+  @Get('table')
+  async listarTable(@Query() paginacionQueryDto: PaginacionQueryDto) {
+    const result = await this.rolService.listarTable(paginacionQueryDto)
+    return this.successListRows(result)
+  }
+
+  @Post()
+  async crear(@Req() req, @Body() rolDto: CrearRolDto) {
+    const usuarioAuditoria = this.getUser(req)
+    const result = await this.rolService.crear(rolDto, usuarioAuditoria)
+    return this.successCreate(result)
+  }
+
+  @Patch(':id')
+  async actualizar(
+    @Param() params: ParamIdDto,
+    @Req() req,
+    @Body() rolDto: CrearRolDto
+  ) {
+    const { id: idRol } = params
+    const usuarioAuditoria = this.getUser(req)
+    const result = await this.rolService.actualizar(
+      idRol,
+      rolDto,
+      usuarioAuditoria
+    )
+    return this.successUpdate(result)
+  }
+
+  @Patch('/:id/activacion')
+  async activar(@Req() req, @Param() params: ParamIdDto) {
+    const { id: idRol } = params
+    const usuarioAuditoria = this.getUser(req)
+    const result = await this.rolService.activar(idRol, usuarioAuditoria)
+    return this.successUpdate(result)
+  }
+
+  @Patch('/:id/inactivacion')
+  async inactivar(@Req() req, @Param() params: ParamIdDto) {
+    const { id: idRol } = params
+    const usuarioAuditoria = this.getUser(req)
+    const result = await this.rolService.inactivar(idRol, usuarioAuditoria)
+    return this.successUpdate(result)
   }
 }
--- src/core/authorization/repository/rol.repository.ts
+++ src/core/authorization/repository/rol.repository.ts
@@ -1,7 +1,9 @@
-import { DataSource } from 'typeorm'
+import { DataSource, UpdateResult } from 'typeorm'
 import { Status } from '../../../common/constants'
 import { Injectable } from '@nestjs/common'
 import { Rol } from '../entity/rol.entity'
+import { CrearRolDto } from '../dto/crear-rol.dto'
+import { PaginacionQueryDto } from 'src/common/dto/paginacion-query.dto'
 
 @Injectable()
 export class RolRepository {
@@ -11,9 +13,27 @@ export class RolRepository {
     return await this.dataSource
       .getRepository(Rol)
       .createQueryBuilder('rol')
-      .select(['rol.id', 'rol.rol', 'rol.nombre'])
+      .select(['rol.id', 'rol.rol', 'rol.nombre', 'rol.estado'])
       .where({ estado: Status.ACTIVE })
       .getMany()
+  }
+
+  async listarTable(paginacionQueryDto: PaginacionQueryDto) {
+    const { limite, saltar, filtro } = paginacionQueryDto
+    const query = await this.dataSource
+      .getRepository(Rol)
+      .createQueryBuilder('rol')
+      .select(['rol.id', 'rol.rol', 'rol.nombre', 'rol.estado'])
+      .orderBy('rol.id', 'ASC')
+      .take(limite)
+      .skip(saltar)
+
+    if (filtro) {
+      query.andWhere('(rol.nombre ilike :filtro or rol.rol ilike :filtro)', {
+        filtro: `%${filtro}`,
+      })
+    }
+    return await query.getManyAndCount()
   }
 
   async buscarPorNombreRol(rol: string) {
@@ -31,5 +51,38 @@ export class RolRepository {
       .select(['rol.id', 'rol.rol'])
       .where({ estado: Status.ACTIVE, usuarioRol: idUsuario })
       .getMany()
+  }
+
+  async buscarPorId(id: string): Promise<Rol | null> {
+    return await this.dataSource
+      .getRepository(Rol)
+      .createQueryBuilder('rol')
+      .where({ id: id })
+      .getOne()
+  }
+
+  async crear(rolDto: CrearRolDto, usuarioAuditoria: string) {
+    const { nombre, rol } = rolDto
+
+    const nuevoRol = new Rol()
+    nuevoRol.nombre = nombre
+    nuevoRol.rol = rol
+    nuevoRol.usuarioCreacion = usuarioAuditoria
+
+    return await this.dataSource.getRepository(Rol).save(nuevoRol)
+  }
+
+  async actualizar(
+    id: string,
+    rolDto: CrearRolDto,
+    usuarioAuditoria: string
+  ): Promise<UpdateResult> {
+    return await this.dataSource.getRepository(Rol).update(
+      id,
+      new Rol({
+        ...rolDto,
+        ...{ usuarioModificacion: usuarioAuditoria },
+      })
+    )
   }
 }
