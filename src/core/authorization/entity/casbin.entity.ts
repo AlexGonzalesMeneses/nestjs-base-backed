04b69b0c7f1f2954ccf15dc8189daa75cc6a622c
Guerrero <samuel.guerrero@agetic.gob.bo>
2023-06-19 12:05:40 -0400

    fix: se define comentarios de campos de casbin

--- src/core/authorization/entity/casbin.entity.ts
+++ src/core/authorization/entity/casbin.entity.ts
@@ -4,54 +4,64 @@ dotenv.config()
 
 @Entity({ schema: process.env.DB_SCHEMA_USUARIOS })
 export class CasbinRule extends BaseEntity {
-  @PrimaryGeneratedColumn()
+  @PrimaryGeneratedColumn({
+    comment: 'Clave primaria de la tabla CasbinRule',
+  })
   public id: number
 
   @Column({
     nullable: true,
     type: 'varchar',
+    comment: 'Tipo de parametro',
   })
   public ptype: string | null
 
   @Column({
     nullable: true,
     type: 'varchar',
+    comment: 'Se define roles',
   })
   public v0: string | null
 
   @Column({
     nullable: true,
     type: 'varchar',
+    comment: 'Se define Rutas',
   })
   public v1: string | null
 
   @Column({
     nullable: true,
     type: 'varchar',
+    comment: 'Se define Permisos de CRUD(Create, Read, Update, Delete)',
   })
   public v2: string | null
 
   @Column({
     nullable: true,
     type: 'varchar',
+    comment: 'Se define proyecto (Backend, Frontend)',
   })
   public v3: string | null
 
   @Column({
     nullable: true,
     type: 'varchar',
+    comment: 'Variables de control',
   })
   public v4: string | null
 
   @Column({
     nullable: true,
     type: 'varchar',
+    comment: 'Variables de control',
   })
   public v5: string | null
 
   @Column({
     nullable: true,
     type: 'varchar',
+    comment: 'Variables de control',
   })
   public v6: string | null
 
