3d1e2746a04eb11eb830030320af3a2713d79aab
Ion Villarreal <ivillarreal@agetic.gob.bo>
2023-07-09 08:54:00 -0400

    fix: corrección de codeSmell en filtro de roles para payload en función de refreshToken

--- src/core/authentication/service/refreshTokens.service.ts
+++ src/core/authentication/service/refreshTokens.service.ts
@@ -22,7 +22,6 @@ dotenv.config()
 
 @Injectable()
 export class RefreshTokensService extends BaseService {
-  // eslint-disable-next-line max-params
   constructor(
     @Inject(RefreshTokensRepository)
     private refreshTokensRepository: RefreshTokensRepository,
@@ -71,12 +70,7 @@ export class RefreshTokensService extends BaseService {
       refreshToken.grantId
     )
 
-    const roles: Array<string | null> = []
-    if (usuario.roles.length) {
-      usuario.roles.map((usuarioRol) => {
-        roles.push(usuarioRol.rol)
-      })
-    }
+    const roles = usuario.roles.map((rol) => rol.rol)
 
     const payload = { id: usuario.id, roles }
     const data = {
