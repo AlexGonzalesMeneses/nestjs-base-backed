a8ca1ce132984565cad4b8e2fabefde5edcc7041
Ion Villarreal <ivillarreal@agetic.gob.bo>
2023-07-18 11:27:43 -0400

    feat: añadido tipo genérico que servira para inferir el tipo de respuesta en controladores

--- src/core/authorization/controller/authorization.service.ts
+++ src/core/authorization/controller/authorization.service.ts
@@ -4,12 +4,14 @@ import { AuthZManagementService } from 'nest-authz'
 import { FiltrosPoliticasDto } from '../dto/filtros-politicas.dto'
 import { ModuloService } from '../service/modulo.service'
 
-interface politicaType {
+export interface politicaType {
   sujeto: string
   objeto: string
   accion: string
   app: string
 }
+
+type politicasResultType = [Array<politicaType>, number]
 
 @Injectable()
 export class AuthorizationService extends BaseService {
@@ -20,7 +22,9 @@ export class AuthorizationService extends BaseService {
     super()
   }
 
-  async listarPoliticas(@Query() paginacionQueryDto: FiltrosPoliticasDto) {
+  async listarPoliticas(
+    @Query() paginacionQueryDto: FiltrosPoliticasDto
+  ): Promise<politicasResultType> {
     const { limite, pagina, filtro, aplicacion, orden, descendente } =
       paginacionQueryDto
 
