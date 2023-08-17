893788ae70b9f925f4d5e66beaa479120c355cec
Alex Quispe <wilmer.quispe@agetic.gob.bo>
2023-08-17 16:08:07 -0400

    fix: :bug: cambiando a rutas absolutas en el stack del error

--- src/core/logger/utilities/get-error-stack.ts
+++ src/core/logger/utilities/get-error-stack.ts
@@ -1,30 +1,6 @@
-import { LoggerService } from '../classes'
-
-const ignoreStackPaths = [
-  './node_modules',
-  './src/driver',
-  './src/query-builder',
-  './src/entity-manager',
-  './src/core/logger',
-  './src/common/exceptions',
-]
-
 export function getErrorStack(error: Error) {
   try {
-    const loggerParams = LoggerService.getLoggerParams()
-    const projectPath = loggerParams?.projectPath
-    const customErrorStack = String(error.stack)
-      .split('\n')
-      .map((line) =>
-        projectPath ? line.replace(new RegExp(projectPath, 'g'), '.') : line
-      )
-      .filter(
-        (line) =>
-          line.includes('./') && !ignoreStackPaths.some((x) => line.includes(x))
-      )
-      .map((line) => line.substring(line.indexOf('./'), line.length - 1))
-      .join('\n')
-      .trim()
+    const customErrorStack = String(error.stack).trim()
     return customErrorStack || ''
   } catch (err) {
     return error.stack || ''
@@ -33,15 +9,7 @@ export function getErrorStack(error: Error) {
 
 export function getFullErrorStack(error: Error) {
   try {
-    const loggerParams = LoggerService.getLoggerParams()
-    const projectPath = loggerParams?.projectPath
-    const customErrorStack = String(error.stack)
-      .split('\n')
-      .map((line) =>
-        projectPath ? line.replace(new RegExp(projectPath, 'g'), '.') : line
-      )
-      .join('\n')
-      .trim()
+    const customErrorStack = String(error.stack).trim()
     return customErrorStack || ''
   } catch (err) {
     return error.stack || ''
