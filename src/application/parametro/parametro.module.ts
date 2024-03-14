b6924330cfba5d37fc97c26bfb4b941c67518f88
Luis <alfredochoque.c@gmail.com>
2024-03-14 11:51:33 -0400

    feat: aplicando alias en las importaciones del módulo Parametro

--- src/application/parametro/parametro.module.ts
+++ src/application/parametro/parametro.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common'
-import { ParametroController } from './controller'
-import { ParametroService } from './service'
+import { ParametroController } from '@/application/parametro/controller/parametro.controller'
+import { ParametroService } from '@/application/parametro/service/parametro.service'
 import { TypeOrmModule } from '@nestjs/typeorm'
-import { ParametroRepository } from './repository'
-import { Parametro } from './entity'
+import { ParametroRepository } from '@/application/parametro/repository/parametro.repository'
+import { Parametro } from '@/application/parametro/entity/parametro.entity'
 
 @Module({
   controllers: [ParametroController],
   providers: [ParametroService, ParametroRepository],
   imports: [TypeOrmModule.forFeature([Parametro])],
 })
-export class ParametroModule {}
+export class ParametroModule { }
